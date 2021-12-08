3a67cad822cea5c65ea1651390dc3f9ddc9400e8
Dae-won Gong <dae-wongong@Dae-wons-MacBook-Pro.local>
2021-12-08 18:00:34 -0500

    Add comments for shiny code.

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,5 +1,8 @@
 source("modals.R")
+
+# UI for shiny application
 ui <- fluidPage(
+  # Styling for shiny error/warning notifications
   tags$head(tags$style(
     HTML(".shiny-notification {
              position:fixed;
@@ -8,24 +11,26 @@ ui <- fluidPage(
              }
              ")
   )),
-  # App title
+  # Application title
   titlePanel(tags$h1(tags$b("NanoPlotR"))),
-  # Sidebar layout
   sidebarLayout(
-    # Sidebar panel
     sidebarPanel(
       fileInput("modResults",
                 "Modification result file:",
                 accept = ".csv, .rds"),
+      # Allow users to select which plots to display.
       checkboxGroupInput("plotSelection", "Select the plots to display:",
                          c("Top Kmers" = "topKmers",
                            "Count Matrix" = "countMatrix",
                            "Histogram" = "hist")),
       hr(),
+      # Start button.
       actionButton(inputId = "start", label = "Start!"),
       br()
     ),
     mainPanel(
+      # tabsetPanel for plots, no panels added here to dynamically update
+      # according to user selection.
       tabsetPanel(
         id = "main_tab"
       )
@@ -41,8 +46,11 @@ server <- function(input, output) {
   plotSelection <- reactiveVal(NULL)
   modResults <- reactiveVal(NULL)
 
+  # Server logic for when user clicks Start button.
   observeEvent(input$start, {
+    # Read input file
     input_file <- input$modResults
+    # Display error if user starts without uploading input file.
     if (is.null(input_file)) {
       error_message <- "Please Input modification result file."
       showNotification(error_message, type = "error")
@@ -50,13 +58,17 @@ server <- function(input, output) {
     }
 
     plotSelection(input$plotSelection)
+    # Display error if user doesn't select any plots to display.
     if (is.null(plotSelection)) {
       error_message <- "Please select atleast 1 plot to begin"
       showNotification(error_message, type = "error")
       return(NULL)
     }
+
     inputFilePath <- input_file$datapath
+    # Logic for reading csv input.
     if (tools::file_ext(inputFilePath) == "csv") {
+      # trycatch for possible read.csv errors caused by incorrect input.
       tryCatch({
         modResults(read.csv(inputFilePath))
       },
@@ -69,7 +81,9 @@ server <- function(input, output) {
       })
     }
 
+    # Logic for reading rds input.
     if (tools::file_ext(inputFilePath) == "rds") {
+      # tryCatch for possible read.csv errors caused by incorrect input
       tryCatch({
         modResults(readRDS(inputFilePath))
       },
@@ -81,17 +95,25 @@ server <- function(input, output) {
         return(NULL)
       })
     }
+
+    # Display appropriate option selection modal depending on user selection
+    # Always show topKmersModal first if user selected. If user did not select
+    # topKmers, show countMatrixModal.
     if ("topKmers" %in% plotSelection()) {
       shiny::showModal(topKmersModal())
     } else if ("countMatrix" %in% plotSelection()){
       shiny::showModal(countMatrixModal())
     }
+
+    # Histogram requires no modals.
     if ("hist" %in% plotSelection()) {
+      # Dynamically insert tab if user selected hist.
       insertTab(
         inputId = "main_tab",
         tab = tabPanel("Histogram",
                        plotOutput("histPlot"))
       )
+      # Render histogram plot.
       output$histPlot <- renderPlot({
         plotModHist(modResults())
       })
@@ -99,36 +121,43 @@ server <- function(input, output) {
   })
 
   observeEvent(input$top_kmer_next, {
+    # Store user input for numKmers
     numKmers(input$numKmers)
+    # Dynamically insert tab if user selected topKmers.
     insertTab(
       inputId = "main_tab",
       tab = tabPanel("Top Kmers Bar Plot",
                      plotOutput("topKmersPlot"))
     )
     removeModal()
+    # Render top kmers plot.
     output$topKmersPlot <- renderPlot({
       plotTopKmers(modResults(), numKmers())
     })
+    # If user also selected countMatrix, show the matrix option modal next.
     if ("countMatrix" %in% plotSelection()) {
       showModal(countMatrixModal())
     }
   })
 
   observeEvent(input$matrix_next, {
+    # Store user input options for countMatrix.
     matrixNumTopIds(input$matrixNumTopIds)
     matrixModSites(input$matrixModSites)
+    # Dynamically insert tab if user selected countMatrix.
     insertTab(
       inputId = "main_tab",
       tab = tabPanel("Count Matrix",
                      plotOutput("countMatrixPlot"))
     )
+    # Render countMatrix plot.
     output$countMatrixPlot <- renderPlot({
       plotCountMatrix(modResults(), matrixModSites(), as.integer(matrixNumTopIds()))
     })
     removeModal()
+    # No more modals needed, additional modals required when more plots supported.
   })
 }
-
 
 # Create Shiny app ----
 shiny::shinyApp(ui = ui, server = server)
--- inst/shiny-scripts/modals.R
+++ inst/shiny-scripts/modals.R
@@ -1,11 +1,16 @@
+# Shiny UI modal components for NanoPlotR
+
+# Modal for user input options for plotting count matrix.
 countMatrixModal <- function(failed = FALSE) {
   modalDialog(
     title = "Count Matrix Options",
+    # Ask user to input desired modification sites.
     checkboxGroupInput("matrixModSites", "Select modification sites:",
                        c("A" = "A",
                          "C" = "C",
                          "G" = "G",
                          "T" = "T")),
+    # Ask user to input number of top kmers to display.
     numericInput(
       "matrixNumTopIds",
       "Input number of top ids to display:",
@@ -13,6 +18,7 @@ countMatrixModal <- function(failed = FALSE) {
       min = 1,
       max = 100,
     ),
+    # Error handling.
     if (failed) {
       div(tags$b("Error", style = "color: red;"))
     },
@@ -23,9 +29,11 @@ countMatrixModal <- function(failed = FALSE) {
   )
 }
 
+# Modal for user input options for plotting top kmers.
 topKmersModal <- function(failed = FALSE) {
   modalDialog(
     title = "Top Kmers Options",
+    # Ask user to input number of top kmers to display.
     numericInput(
       "numKmers",
       "Input number of top kmers to display:",
@@ -33,6 +41,7 @@ topKmersModal <- function(failed = FALSE) {
       min = 1,
       max = 100,
     ),
+    # Error handling.
     if (failed) {
       div(tags$b("Error", style = "color: red;"))
     },
